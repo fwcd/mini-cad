fa45e669899310c58c03e88bda96611de5f45a72
fwcd <fwcdmail@gmail.com>
2023-01-21 20:38:49 +0100

    Set up some light

--- MiniSlice.swiftpm/Sources/ViewModel/StageViewModel.swift
+++ MiniSlice.swiftpm/Sources/ViewModel/StageViewModel.swift
@@ -5,11 +5,26 @@ class StageViewModel: ObservableObject {
     @Published var scene = SCNScene(named: "Stage.scn")!
     
     init() {
-        let cameraNode = SCNNode()
         let camera = SCNCamera()
+        let cameraNode = SCNNode()
         cameraNode.camera = camera
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 5)
         scene.rootNode.addChildNode(cameraNode)
+        
+        let dirLight = SCNLight()
+        dirLight.type = .directional
+        let dirLightNode = SCNNode()
+        dirLightNode.light = dirLight
+        dirLightNode.eulerAngles.x = .pi / 8
+        dirLightNode.eulerAngles.y = .pi / 8
+        scene.rootNode.addChildNode(dirLightNode)
+        
+        let ambientLight = SCNLight()
+        ambientLight.type = .ambient
+        ambientLight.color = UIColor.gray
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = ambientLight
+        scene.rootNode.addChildNode(ambientLightNode)
         
         let cubeBox = SCNBox()
         cubeBox.firstMaterial?.diffuse.contents = UIColor.orange
