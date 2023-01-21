e9af6ae1564db6a23aecb8ddb44574235ce16aeb
fwcd <fwcdmail@gmail.com>
2023-01-21 23:48:24 +0100

    Fix demo recipe

--- MiniSlice.swiftpm/Sources/Model/Recipe/DemoRecipe.swift
+++ MiniSlice.swiftpm/Sources/Model/Recipe/DemoRecipe.swift
@@ -2,5 +2,5 @@ let demoRecipe = Recipe(statements: [
     .varBinding(.init(name: "x", value: 3)),
     .varBinding(.init(name: "y", value: 4.3)),
     .blank,
-    .expression(.call("Cube", [])),
+    .expression(.call("Cuboid", [])),
 ])
