247589506e25b393bb71f464a549f0d1cc869ed7
fwcd <fwcdmail@gmail.com>
2023-01-22 01:56:52 +0100

    Add todo notes

--- MiniSlice.swiftpm/Sources/Model/Recipe/Recipe.swift
+++ MiniSlice.swiftpm/Sources/Model/Recipe/Recipe.swift
@@ -1,6 +1,7 @@
 struct Recipe: Hashable, CustomStringConvertible {
     var statements: [Statement] = []
     
+    // TODO: Add separate format method and pass indent
     var description: String {
         statements.map { "\($0)" }.joined(separator: "\n")
     }
--- MiniSlice.swiftpm/Sources/View/StageView.swift
+++ MiniSlice.swiftpm/Sources/View/StageView.swift
@@ -6,6 +6,7 @@ struct StageView: View {
     
     var body: some View {
         ZStack(alignment: .topTrailing) {
+            // TODO: Zoom when scrolling, do we need to implement a custom SCNSceneView wrapper for this?
             SceneView(
                 scene: viewModel.scene,
                 pointOfView: viewModel.cameraNode,
