07f4dff6444fa74965131f0badbaf59c247f4880
fwcd <fwcdmail@gmail.com>
2023-02-07 23:48:10 +0100

    Use constant color for axis

--- MiniSlice.swiftpm/Sources/ViewModel/Node/AxisNode.swift
+++ MiniSlice.swiftpm/Sources/ViewModel/Node/AxisNode.swift
@@ -7,7 +7,17 @@ class AxisNode: SCNNode {
         let thickness: CGFloat = 0.01
         let length: CGFloat = 100.0
         let boxAxis = Vec3(z: 1)
+        
+        let material = SCNMaterial()
+        let color = UIColor.gray
+        material.lightingModel = .constant
+        material.ambient.contents = UIColor.black
+        material.diffuse.contents = color
+        material.emission.contents = color
+        
         let box = SCNBox(width: thickness, height: thickness, length: length, chamferRadius: thickness / 2)
+        box.materials = [material]
+        
         let child = SCNNode(geometry: box)
         child.rotation = SCNVector4(direction.cross(boxAxis), w: .pi / 2)
         addChildNode(child)
