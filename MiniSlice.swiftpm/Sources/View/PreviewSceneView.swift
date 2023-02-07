fa4028609eb22018ee8740017b0da8ecb6c3c452
fwcd <fwcdmail@gmail.com>
2023-02-08 00:22:32 +0100

    Add experimental custom camera control

--- MiniSlice.swiftpm/Sources/View/PreviewSceneView.swift
+++ MiniSlice.swiftpm/Sources/View/PreviewSceneView.swift
@@ -5,9 +5,7 @@ struct PreviewSceneView: UIViewRepresentable {
     @EnvironmentObject var viewModel: PreviewViewModel
     
     func makeUIView(context: Context) -> SCNView {
-        let uiView = SCNView()
-        uiView.allowsCameraControl = true
-        return uiView
+        SCNView()
     }
     
     func updateUIView(_ uiView: SCNView, context: Context) {
