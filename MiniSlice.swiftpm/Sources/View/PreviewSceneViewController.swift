e2599482fe129b02486e1562c2fabd5d540feda8
fwcd <fwcdmail@gmail.com>
2023-02-08 01:04:55 +0100

    Implement custom controls

--- MiniSlice.swiftpm/Sources/View/PreviewSceneViewController.swift
+++ MiniSlice.swiftpm/Sources/View/PreviewSceneViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import SceneKit
 
-class PreviewSceneViewController: UIViewController {
+class PreviewSceneViewController: UIViewController, UIGestureRecognizerDelegate {
     var scene: SCNScene? {
         didSet {
             sceneView.scene = scene
@@ -13,10 +13,41 @@ class PreviewSceneViewController: UIViewController {
         }
     }
     
+    private var panRecognizer: UIPanGestureRecognizer!
+    private var scrollRecognizer: UIPanGestureRecognizer!
+    private var panSensitivity: Double = 0.0001
+    private var zoomSensitivity: Double = 0.0002
     private var sceneView: SCNView!
     
     override func loadView() {
         sceneView = SCNView()
+        
+        panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        panRecognizer.delegate = self
+        sceneView.addGestureRecognizer(panRecognizer)
+        
+        scrollRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handleScroll))
+        scrollRecognizer.delegate = self
+        scrollRecognizer.allowedScrollTypesMask = .all
+        scrollRecognizer.allowedTouchTypes = []
+        sceneView.addGestureRecognizer(scrollRecognizer)
+        
         view = sceneView
+    }
+    
+    @objc
+    private func handlePan(_ recognizer: UIPanGestureRecognizer) {
+        let delta = recognizer.velocity(in: view)
+        
+        let controller = sceneView.defaultCameraController
+        controller.rotateBy(x: Float(-delta.x * panSensitivity), y: -Float(delta.y * panSensitivity))
+    }
+    
+    @objc
+    private func handleScroll(_ recognizer: UIPanGestureRecognizer) {
+        let delta = recognizer.velocity(in: view)
+        
+        let controller = sceneView.defaultCameraController
+        controller.dolly(toTarget: -Float(delta.y * zoomSensitivity))
     }
 }
--- MiniSlice.swiftpm/Sources/View/PreviewView.swift
+++ MiniSlice.swiftpm/Sources/View/PreviewView.swift
@@ -2,30 +2,9 @@ import SwiftUI
 import SceneKit
 
 struct PreviewView: View {
-    @EnvironmentObject var viewModel: PreviewViewModel
-    var dragSensitivity: Double = 1
-    
-    @State private var initialEulerAngles: SCNVector3? = nil
-    
     var body: some View {
         ZStack(alignment: .topTrailing) {
-            // TODO: Zoom when scrolling, do we need to implement a custom SCNSceneView wrapper for this?
             PreviewSceneView()
-                .gesture(
-                    DragGesture()
-                        .onChanged { value in
-                            let factor = 0.001
-                            
-                            if let cameraNode = viewModel.cameraNode {
-                                let initialEulerAngles = initialEulerAngles ?? cameraNode.eulerAngles
-                                
-                                cameraNode.eulerAngles.x = initialEulerAngles.x + Float(value.translation.height * dragSensitivity * factor)
-                                cameraNode.eulerAngles.y = initialEulerAngles.y + Float(value.translation.width * dragSensitivity * factor)
-                                
-                                self.initialEulerAngles = initialEulerAngles
-                            }
-                        }
-                )
             OptionsView()
                 .frame(maxWidth: 300)
                 .padding(10)
