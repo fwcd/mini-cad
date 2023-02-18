808b99544d62d93f3202e38ef2d32410e1f47925
fwcd <fwcdmail@gmail.com>
2023-02-18 17:20:42 +0100

    Use BinaryExpression convenience factories in demoRecipe again

--- MiniSlice.swiftpm/Sources/Model/Recipe/DemoRecipe.swift
+++ MiniSlice.swiftpm/Sources/Model/Recipe/DemoRecipe.swift
@@ -1,12 +1,12 @@
-let demoRecipe: Recipe = Recipe(statements: [
+let demoRecipe: Recipe = [
     .varBinding(.init(name: "w", value: 3)),
     .varBinding(.init(name: "h", value: 4)),
     .blank,
-    .forLoop(.init(name: "i", sequence: .binary(.init(lhs: 0, op: .toExclusive, rhs: "w")), block: [
-        .forLoop(.init(name: "j", sequence: .binary(.init(lhs: 0, op: .toExclusive, rhs: "h")), block: [
+    .forLoop(.init(name: "i", sequence: .binary(.range(0, "w")), block: [
+        .forLoop(.init(name: "j", sequence: .binary(.range(0, "h")), block: [
             .expression(.call(.init(identifier: "Translate", args: ["i", "i", "j"], trailingBlock: [
                 .expression(.call("Cuboid")),
             ]))),
         ])),
     ])),
-])
+]
