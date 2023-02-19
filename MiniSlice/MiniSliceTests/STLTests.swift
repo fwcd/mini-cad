7e1535af38cb8897ec9d253aa590ba3c312d402b
fwcd <fwcdmail@gmail.com>
2023-02-19 18:43:31 +0100

    Add short STL generation test

--- MiniSlice/MiniSliceTests/STLTests.swift
+++ MiniSlice/MiniSliceTests/STLTests.swift
@@ -3,6 +3,94 @@ import XCTest
 
 final class STLTests: XCTestCase {
     func testCuboidSTL() {
-        print(Mesh(Cuboid()).asSTL)
+        // We're comparing floating-points for exact equality here, but since floating-point arithmetic should be deterministic, this is still a quick and useful way to pin down the resulting STL and make sure that we are not regressing on anything
+        XCTAssertEqual(Mesh(Cuboid()).asSTL, """
+            solid mesh
+                facet normal 1.0 -0.0 0.0
+                    outer loop
+                        vertex -0.5 -0.5 -0.5
+                        vertex -0.5 -0.5 0.5
+                        vertex -0.5 0.5 0.5
+                    endloop
+                endfacet
+                facet normal 1.0 0.0 0.0
+                    outer loop
+                        vertex -0.5 -0.5 -0.5
+                        vertex -0.5 0.5 0.5
+                        vertex -0.5 0.5 -0.5
+                    endloop
+                endfacet
+                facet normal -1.0 -0.0 -0.0
+                    outer loop
+                        vertex 0.5 0.5 -0.5
+                        vertex 0.5 0.5 0.5
+                        vertex 0.5 -0.5 0.5
+                    endloop
+                endfacet
+                facet normal -1.0 0.0 0.0
+                    outer loop
+                        vertex 0.5 0.5 -0.5
+                        vertex 0.5 -0.5 0.5
+                        vertex 0.5 -0.5 -0.5
+                    endloop
+                endfacet
+                facet normal 0.0 1.0 0.0
+                    outer loop
+                        vertex 0.5 -0.5 -0.5
+                        vertex 0.5 -0.5 0.5
+                        vertex -0.5 -0.5 0.5
+                    endloop
+                endfacet
+                facet normal 0.0 1.0 0.0
+                    outer loop
+                        vertex 0.5 -0.5 -0.5
+                        vertex -0.5 -0.5 0.5
+                        vertex -0.5 -0.5 -0.5
+                    endloop
+                endfacet
+                facet normal 0.0 -1.0 0.0
+                    outer loop
+                        vertex -0.5 0.5 -0.5
+                        vertex -0.5 0.5 0.5
+                        vertex 0.5 0.5 0.5
+                    endloop
+                endfacet
+                facet normal 0.0 -1.0 -0.0
+                    outer loop
+                        vertex -0.5 0.5 -0.5
+                        vertex 0.5 0.5 0.5
+                        vertex 0.5 0.5 -0.5
+                    endloop
+                endfacet
+                facet normal 0.0 -0.0 1.0
+                    outer loop
+                        vertex 0.5 0.5 -0.5
+                        vertex 0.5 -0.5 -0.5
+                        vertex -0.5 -0.5 -0.5
+                    endloop
+                endfacet
+                facet normal 0.0 0.0 1.0
+                    outer loop
+                        vertex 0.5 0.5 -0.5
+                        vertex -0.5 -0.5 -0.5
+                        vertex -0.5 0.5 -0.5
+                    endloop
+                endfacet
+                facet normal -0.0 -0.0 -1.0
+                    outer loop
+                        vertex 0.5 -0.5 0.5
+                        vertex 0.5 0.5 0.5
+                        vertex -0.5 0.5 0.5
+                    endloop
+                endfacet
+                facet normal 0.0 0.0 -1.0
+                    outer loop
+                        vertex 0.5 -0.5 0.5
+                        vertex -0.5 0.5 0.5
+                        vertex -0.5 -0.5 0.5
+                    endloop
+                endfacet
+            endsolid mesh
+            """)
     }
 }
