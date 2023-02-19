70b5fd259a9df4635298a4b46011a90c47166b80
fwcd <fwcdmail@gmail.com>
2023-02-19 03:48:25 +0100

    Sync document in ContentView

--- MiniSlice.swiftpm/Sources/View/ContentView.swift
+++ MiniSlice.swiftpm/Sources/View/ContentView.swift
@@ -1,10 +1,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Binding var document: RecipeDocument
+    @EnvironmentObject private var editor: EditorViewModel
+    
     var body: some View {
         HStack {
             EditorView()
             PreviewView()
+        }
+        .onAppear {
+            // TODO: Is this sufficient? The user should be able to open documents, which might not cause the ContentView to 're-appear' but only update? We don't listen to document changes directly to prevent a listener cycle.
+            editor.rawRecipe = document.raw
+        }
+        .onChange(of: editor.rawRecipe) { raw in
+            document.raw = raw
         }
     }
 }
