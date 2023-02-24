b36cb0a4e9dbde417f4f55a41409baae730786ee
fwcd <fwcdmail@gmail.com>
2023-02-24 18:10:27 +0100

    Add insertion method for octree

--- MiniSlice.swiftpm/Sources/Model/Geometry/CSG/TriangleOctree.swift
+++ MiniSlice.swiftpm/Sources/Model/Geometry/CSG/TriangleOctree.swift
@@ -8,7 +8,7 @@ struct TriangleOctree {
                 assert(isOctree)
             }
         }
-    
+        
         private var isOctree: Bool {
             children.isEmpty || children.count == 8
         }
@@ -18,17 +18,30 @@ struct TriangleOctree {
             self.children = children
             assert(isOctree)
         }
+        
+        mutating func insert(triangle: Triangle, aabb: AxisAlignedBoundingBox, remainingDepth: Int) {
+            if remainingDepth <= 0 {
+                let octants = aabb.octants
+                for (i, octant) in octants.enumerated() {
+                    if octant.fullyContains(triangle) {
+                        children[i].insert(triangle: triangle, aabb: octant, remainingDepth: remainingDepth - 1)
+                        return
+                    }
+                }
+            }
+            triangles.append(triangle)
+        }
     }
     
-    mutating func insert(triangle: Triangle, aabb: AxisAlignedBoundingBox) {
-        
+    mutating func insert(triangle: Triangle, aabb: AxisAlignedBoundingBox, maxDepth: Int) {
+        root.insert(triangle: triangle, aabb: aabb, remainingDepth: maxDepth)
     }
 }
 
 extension TriangleOctree {
-    init(mesh: Mesh) {
+    init(mesh: Mesh, aabb: AxisAlignedBoundingBox, maxDepth: Int = 8) {
         for triangle in mesh {
-            
+            insert(triangle: triangle, aabb: aabb, maxDepth: maxDepth)
         }
     }
 }
