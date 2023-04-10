5f8699cb06ca0bae2267c6ef9afa56a179fa1012
fwcd <fwcdmail@gmail.com>
2023-04-10 18:08:18 +0200

    Test plane normals

--- MiniCAD/MiniCADTests/PlaneTests.swift
+++ MiniCAD/MiniCADTests/PlaneTests.swift
@@ -20,4 +20,18 @@ final class PlaneTests: XCTestCase {
         XCTAssertEqual(point.y, 1 / 3, accuracy: epsilon)
         XCTAssertEqual(point.z, 1 / 3, accuracy: epsilon)
     }
+    
+    func testPlaneNormal() {
+        assertThat(Plane(a: Vec3(x: 2.6481), b: Vec3(y: 7.5410), c: Vec3(z: 1.7694)).unitNormal, approximatelyEquals: Vec3(x: -0.5453, y: -0.1915, z: -0.8161).normalized)
+        assertThat(Plane(a: Vec3(x: -2.6481), b: Vec3(y: 7.5410), c: Vec3(z: 1.7694)).unitNormal, approximatelyEquals: Vec3(x: -0.5453, y: 0.1915, z: 0.8161).normalized)
+        assertThat(Plane(a: Vec3(x: -1.7694), b: Vec3(y: 7.5410), c: Vec3(z: 2.6481)).unitNormal, approximatelyEquals: Vec3(x: -0.8161, y: 0.1915, z: 0.5453).normalized)
+        assertThat(Plane(a: Vec3(x: -1.7694), b: Vec3(y: -7.5410), c: Vec3(z: 2.6481)).unitNormal, approximatelyEquals: Vec3(x: 0.8161, y: 0.1915, z: -0.5453).normalized)
+        assertThat(Plane(a: Vec3(x: -2.6481), b: Vec3(y: -7.5410), c: Vec3(z: -1.7694)).unitNormal, approximatelyEquals: Vec3(x: -0.5453, y: -0.1915, z: -0.8161).normalized)
+    }
+    
+    private func assertThat(_ a: Vec3, approximatelyEquals b: Vec3, line: UInt = #line) {
+        XCTAssertEqual(a.x, b.x, accuracy: epsilon, line: line)
+        XCTAssertEqual(a.y, b.y, accuracy: epsilon, line: line)
+        XCTAssertEqual(a.z, b.z, accuracy: epsilon, line: line)
+    }
 }
