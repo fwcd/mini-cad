4a96101ef1ce6469d775d0e8f52d102ed307e14a
fwcd <fwcdmail@gmail.com>
2023-04-10 15:32:25 +0200

    Add the other CSG operations (experimentally)

--- MiniCAD.swiftpm/Sources/Model/Geometry/CSG/Mesh+BooleanOperations.swift
+++ MiniCAD.swiftpm/Sources/Model/Geometry/CSG/Mesh+BooleanOperations.swift
@@ -2,14 +2,57 @@
 // Copyright (c) 2011 Evan Wallace (http://madebyevan.com/)
 
 extension Mesh {
+    /// Return a new CSG solid representing space in either this solid or in the
+    /// solid `csg`. Neither this solid nor the solid `csg` are modified.
+    ///
+    ///     A.union(B)
+    ///
+    ///     +-------+            +-------+
+    ///     |       |            |       |
+    ///     |   A   |            |       |
+    ///     |    +--+----+   =   |       +----+
+    ///     +----+--+    |       +----+       |
+    ///          |   B   |            |       |
+    ///          |       |            |       |
+    ///          +-------+            +-------+
+    ///
     func union(_ rhs: Self) -> Self {
-        // TODO: Implement union
-        return disjointUnion(rhs)
+        var a = BinarySpacePartitioning(inserting: facePolygons)
+        var b = BinarySpacePartitioning(inserting: rhs.facePolygons)
+        a.clip(to: b)
+        b.clip(to: a)
+        b.invert()
+        b.clip(to: a)
+        b.invert()
+        a.insert(polygons: b.allPolygons)
+        return Mesh(a.allPolygons)
     }
     
+    /// Return a new CSG solid representing space both this solid and in the
+    /// solid `csg`. Neither this solid nor the solid `csg` are modified.
+    ///
+    ///     A.intersect(B)
+    ///
+    ///     +-------+
+    ///     |       |
+    ///     |   A   |
+    ///     |    +--+----+   =   +--+
+    ///     +----+--+    |       +--+
+    ///          |   B   |
+    ///          |       |
+    ///          +-------+
+    ///
     func intersection(_ rhs: Self) -> Self {
-        // TODO: Implement intersection
-        return disjointUnion(rhs)
+        var a = BinarySpacePartitioning(inserting: facePolygons)
+        var b = BinarySpacePartitioning(inserting: rhs.facePolygons)
+        a.invert()
+        b.clip(to: a)
+        b.invert()
+        a.clip(to: b)
+        b.clip(to: a)
+        a.insert(polygons: b.allPolygons)
+        a.invert()
+        return Mesh(a.allPolygons)
     }
     
     /// Return a new CSG solid representing space in this solid but not in the
