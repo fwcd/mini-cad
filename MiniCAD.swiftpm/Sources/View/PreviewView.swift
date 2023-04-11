14c71f4f84ef4f1a0213b377d9b3fc0f0198d586
fwcd <fwcdmail@gmail.com>
2023-04-11 17:25:11 +0200

    Make options collapsible

--- MiniCAD.swiftpm/Sources/View/PreviewView.swift
+++ MiniCAD.swiftpm/Sources/View/PreviewView.swift
@@ -2,20 +2,33 @@ import SwiftUI
 import SceneKit
 
 struct PreviewView: View {
-    @EnvironmentObject var viewModel: PreviewViewModel
+    @EnvironmentObject private var viewModel: PreviewViewModel
+    @State private var optionsShown: Bool = true
     
     var body: some View {
         // TODO: Zoom when scrolling, do we need to implement a custom SCNSceneView wrapper for this?
         PreviewSceneView()
             .edgesIgnoringSafeArea(.all)
             .overlay(alignment: .bottomTrailing) {
-                VStack {
-                    OptionsView()
+                VStack(spacing: 5) {
+                    Button {
+                        withAnimation(.spring()) {
+                            optionsShown = !optionsShown
+                        }
+                    } label: {
+                        Image(systemName: optionsShown ? "chevron.down" : "chevron.up")
+                            .foregroundColor(.primary)
+                            .frame(maxWidth: .infinity)
+                    }
+                    .buttonStyle(.borderless)
+                    if optionsShown {
+                        OptionsView()
+                    }
                     PreviewToolbarView()
                 }
-                    .frame(maxWidth: 250)
-                    .modifier(PreviewOverlay())
-                    .padding(ViewConstants.padding)
+                .frame(maxWidth: optionsShown ? 250 : 200)
+                .modifier(PreviewOverlay())
+                .padding(ViewConstants.padding)
             }
     }
 }
