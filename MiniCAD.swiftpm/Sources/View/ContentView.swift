1afaa87128d178214ad0d91248bcce09924e5a8a
fwcd <fwcdmail@gmail.com>
2023-04-11 16:33:01 +0200

    Add sidebar

--- MiniCAD.swiftpm/Sources/View/ContentView.swift
+++ MiniCAD.swiftpm/Sources/View/ContentView.swift
@@ -1,10 +1,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var columnVisibility: NavigationSplitViewVisibility = .all
+    
     var body: some View {
-        HStack {
-            EditorView()
-            PreviewView()
+        NavigationSplitView(columnVisibility: $columnVisibility) {
+            // TODO
+        } detail: {
+            HStack {
+                EditorView()
+                PreviewView()
+            }
+            // TODO: Figure out a more elegant way to integrate the sidebar toggle button
+            .toolbar(columnVisibility == .all ? .hidden : .visible, for: .navigationBar)
         }
     }
 }
